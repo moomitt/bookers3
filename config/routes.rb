13f256c3562e6fdb918c44014f3232bcb5ccbfb3
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-12 02:15:57 +0000

    [8] booksコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
