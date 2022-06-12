65d11100bbb7d618ac859e5f6e7b38f29df4da1f
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-12 03:35:56 +0000

    [13] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   patch 'users/:id' => 'users#update', as: 'update_user'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
