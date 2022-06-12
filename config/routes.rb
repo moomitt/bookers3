71c82a704c7270234bfa10dfba8663947f30e13d
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-12 03:37:33 +0000

    [13] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  patch 'users/:id' => 'users#update', as: 'update_user'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  patch 'users/:id' => 'users#update', as: 'update_user'
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
