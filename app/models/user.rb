19841a45075979749475b9df52206aff018d2011
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-14 03:08:54 +0000

    [38-40] バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,8 @@ class User < ApplicationRecord
       end
       profile_image.variant(resize_to_limit: [width, height]).processed
     end
+    
+  validates :name, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
   
 end
